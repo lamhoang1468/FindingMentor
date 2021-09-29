9f4c9219927ef507b9811585b997dc56fa18ec63
fanglong-it <Fang.longpc@gmail.com>
2021-09-30 00:20:40 +0700

    fanglong - test 12am20

--- src/java/c4/fm/utils/DBUtils.java
+++ src/java/c4/fm/utils/DBUtils.java
@@ -9,10 +9,6 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 /**
  *
@@ -22,8 +18,8 @@ public class DBUtils implements Serializable{
       public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection conn = null;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=FindingMentor";
-        conn = DriverManager.getConnection(url, "nam11", "1");
+        String url = "jdbc:sqlserver://localhost:1434;databaseName=FindingMentor";
+        conn = DriverManager.getConnection(url, "sa", "123456");
         return conn;
     }
 }
